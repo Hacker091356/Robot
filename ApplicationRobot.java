646d144624370c2b58f20c22e1e2afd4741d12c0
Hacker091356 <ninno2812@gmail.com>
2024-05-22 15:00:21 -0400

    Create ApplicationRobot.java

---
+++ ApplicationRobot.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+import  java.awt.event.InputEvent;
+public class ApplicationRobot{
+    public static void main(String[] args) throws Exception{
+        Robot Neil = new Robot();
+        Neil.mouseMove(0,1200);
+        Neil.setAutoDelay(1000);
+        Neil.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        Neil.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        Neil.setAutoDelay(300);
+        Neil.mouseMove(100,720);
+        Neil.setAutoDelay(1000);
+        Neil.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        Neil.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        Neil.setAutoDelay(1000);
+        Neil.mouseMove(1200,500);
+        Neil.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        Neil.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        Neil.setAutoDelay(2000);
+        Neil.mouseMove(1200,800);
+        Neil.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        Neil.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+    }
+}
